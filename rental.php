b4edb15fcd7ac0b69160972c5b926b2f973dc9f8
Philipp Schmiedel <philipp.schmiedel@gmail.com>
2013-09-25 13:06:20 +0200

    - ugly rent cards layout

--- rental.php
+++ rental.php
@@ -1,7 +1,44 @@
 <?php
-    require_once('header.php');
+require_once('php/Database.php');
+require_once('header.php');
 ?>
 
 <?php
-    require_once('footer.php');
+/**
+ * Print rental button
+ *
+ * @param $name
+ * @param $disabled
+ */
+function printButton($name, $disabled)
+{
+    $disabledStr = $disabled ? 'disabled' : '';
+    echo('<div class="span6" align="center"><button style="margin :10px" class="btn btn-info ' . $disabledStr . '">' . $name . '</button></div>');
+}
+
+/**
+ * Get all cards and print buttons
+ */
+function getCards()
+{
+    $db = new Database();
+
+    $result = $db->query('SELECT * FROM `Card` ');
+
+    foreach ($result as $card)
+    {
+        printButton($card['name'], !empty($card['transactionId']));
+    }
+}
+
+?>
+
+    <div class="row">
+        <?php
+        getCards();
+        ?>
+    </div>
+
+<?php
+require_once('footer.php');
 ?>
