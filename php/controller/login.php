ffa37e700dcd94d2fcc4b266b1f78fc48dbf127f
Karl Bergthaler <kbergthaler@intelliad.de>
2013-09-25 12:26:42 +0200

    Add login.php

---
+++ php/controller/login.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once "../Database.php";
+/**
+ *
+ * @author    Karl Bergthaler
+ * @author    Georg Döhring
+ * @copyright Copyright (c) 2013
+ * @license   proprietary
+ */
+
+$username = $_POST['username'];
+$pass = $_POST['pass'];
+
+if (empty($username) OR empty($pass))
+{
+    returnToSignin();
+}
+
+$db = new Database();
+$result = $db->getUserInfo($username);
+
+$userId = $result['userId'];
+$hash = $db->getPassHash($pass);
+
+if (!empty($result) AND $result['password'] === $hash)
+{
+    session_start();
+    $_SESSION['userId'] = $userId;
+    $_SESSION['hash'] = $hash;
+    session_write_close();
+    header("Location: /signin.php?success=1");
+    die;
+    //TODO redirect
+}
+else
+{
+    returnToSignin();
+}
+
+
+
+####Functions
+function returnToSignin()
+{
+    header("Location: /signin.php?error=1");
+    die;
+}
